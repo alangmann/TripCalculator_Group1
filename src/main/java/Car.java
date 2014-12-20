dece9375c47e9f581b9a0c9820320ccd70ff6941
kirluc11 <kirluc11@htlkaindorf.at>
2014-12-20 10:36:40 +0100

    added Car and Truck

---
+++ src/main/java/Car.java
@@ -0,0 +1,5 @@
+public class Car extends Vehicle{
+    public Car(int cargo, String typeOfFuel, double averageConsumption) {
+        super(cargo, typeOfFuel, averageConsumption);
+    }
+}
---
+++ src/main/java/Truck.java
@@ -0,0 +1,11 @@
+public class Truck extends Vehicle{
+
+    private int axles;
+    private boolean adBlue;
+
+    public Truck(int cargo, String typeOfFuel, double averageConsumption, boolean adBlue, int axles) {
+        super(cargo, typeOfFuel, averageConsumption);
+        this.adBlue = adBlue;
+        this.axles = axles;
+    }
+}
