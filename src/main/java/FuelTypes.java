e155d4cb8bd7650d9447b10e145f189ae39e4b89
kirluc11 <kirluc11@htlkaindorf.at>
2014-12-20 10:19:28 +0100

    added Vehicle class added FuelType enum

---
+++ src/main/java/FuelTypes.java
@@ -0,0 +1,6 @@
+public class FuelTypes {
+
+    protected enum FuelType {
+        DIESEL, PATROL
+    }
+}
---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,8 @@
+public class Vehicle extends FuelTypes{
+
+    private double averageConsumption;
+    private FuelType typeOfFuel;
+    private int cargo;
+
+
+}
