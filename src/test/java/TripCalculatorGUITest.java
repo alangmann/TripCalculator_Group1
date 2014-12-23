961c6fd9c7b3137227a258a0152a9123bde9907c
Christoph Minixhofer <minchc11@htlkaindorf.at>
2014-12-24 00:33:54 +0100

    add gui tests

--- src/test/java/TripCalculatorGUITest.java
+++ src/test/java/TripCalculatorGUITest.java
@@ -1,5 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
+import tripcalculator.gui.AddTripDialog;
 import tripcalculator.gui.TripCalculatorGUI;
 
 import static org.junit.Assert.*;
@@ -17,5 +18,12 @@ public class TripCalculatorGUITest {
     public void setVisibleWorks() {
         gui.setVisible(true);
         assertTrue(gui.getWidth() > 0);
+    }
+
+    @Test
+    public void dialogWorks() {
+        AddTripDialog dialog = new AddTripDialog(gui, false);
+        dialog.setVisible(true);
+        assertTrue(dialog.getWidth() > 0);
     }
 }
